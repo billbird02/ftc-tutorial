2f74636786e99e5f259624e88d2d6253fe6c2717
billbird02 <wjbird@gmail.com>
2024-11-26 10:59:34 -0500

    FTC Programming Episode 5.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearTeleOp.java
@@ -1,3 +1,6 @@
+/*
+From FTC Team 7477 Youtube video series: https://youtu.be/ig9YUI4wu6c?si=GKPDrYQiAySjAnxz
+ */
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -8,7 +11,7 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp (name = "First_TeleOp") // imports TeleOp code and sets unique program name on control hub
+@TeleOp (name = "First TeleOp") // adds TeleOp library import and sets program name on control hub
 public class LinearTeleOp extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
@@ -32,14 +35,68 @@ public class LinearTeleOp extends LinearOpMode {
         CRServo servo2; // must use servo programmer to switch servo mode to continuous
         servo2 = hardwareMap.get(CRServo.class, "servo_two");
 
+        // GAME CONTROLLER (Logitech F310)
+        double joystick;
+        boolean isPressed;
+        boolean isDown = true;
+        boolean previousCycle = false, currentCycle = false;
+
         waitForStart(); // waits until start is pressed
 
         while (opModeIsActive()) {
             motor1.setPower(0.5);    // values from -1 to 1 inclusive
             motor2.setVelocity(150);    // set velocity based on ticks per second; more accurate than power
 
+
             servo1.setPosition(0.4);    // values from 0 to 1 inclusive.  sets initial servo position (e.g. 0.5 is halfway in range
             servo2.setPower(0.85);  // values from 0 to 1 inclusive
+
+
+            if (gamepad2.a) {
+                servo1.setPosition(1);
+            }
+            if (gamepad2.b) {
+                servo1.setPosition(0);
+            }
+            joystick = gamepad1.left_stick_x;   // moving joystick LEFT is a negative value and RIGHT is a positive value
+            joystick = gamepad1.left_stick_y;   // moving joystick UP is a negative value and DOWN is a positive value
+                                                // use -gamepad1.left_stick_y (equivalent to gamepad1.left_stick_y * -1) for
+                                                // more intuitive operation where UP is positive and DOWN is negative.
+            motor1.setPower(gamepad1.left_stick_x); // for controlling motor power level
+
+            isPressed = gamepad1.right_trigger > 0.05;
+            if (isPressed) {
+                servo1.setPosition(0.6);
+            }
+
+            /*
+            This code segment will not work due to how quickly the program loops through the while block; it will run
+            through several iterations while the button is pressed and will quickly cycle through the UP and DOWN toggles.
+
+            if (gamepad1.a) {
+                isDown = !isDown;   // toggles state of button 'a' (e.g., false -> true or true -> false)
+                if (isDown) {
+                    servo1.setPosition(0);  // move DOWN
+                } else {
+                    servo1.setPosition(1);  // move UP
+                }
+            }
+
+            The following code segment is what works; need to track current and previous values.
+            */
+
+            previousCycle = currentCycle;
+            currentCycle = gamepad1.a;
+            if (currentCycle && !previousCycle) {   // set leading edge detector; code is only run once when button is pressed
+                                                    // (https://gm0.org/en/latest/docs/software/tutorials/gamepad.html#rising-edge-detector)
+                isDown = !isDown;   // toggles state of button 'a' (e.g., false -> true or true -> false)
+                if (isDown) {
+                    servo1.setPosition(0);  // move DOWN
+                } else {
+                    servo1.setPosition(1);  // move UP
+                }
+            }
+
         }
 
     }
