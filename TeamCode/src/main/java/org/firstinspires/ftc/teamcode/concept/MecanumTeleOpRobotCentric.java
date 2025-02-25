243e7c1eeefc75083eb2bcdafb1ee55f6eb1f7e8
billbird02 <wjbird@gmail.com>
2025-02-25 18:35:52 -0500

    Added speedLimiter parameter to moveRobot().

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/concept/MecanumTeleOpRobotCentric.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/concept/MecanumTeleOpRobotCentric.java
@@ -10,24 +10,25 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name="Mecanum TeleOp (Robot Centric)", group="Concept")
 //@Disabled   // comment this out to add to the OpMode list on the Driver Hub
 public class MecanumTeleOpRobotCentric extends LinearOpMode {
+
+    private DcMotor frontLeftMotor = null;
+    private DcMotor rearLeftMotor = null;
+    private DcMotor frontRightMotor = null;
+    private DcMotor rearRightMotor = null;
 
     @Override
     public void runOpMode() throws InterruptedException {
 
         // Initialize the hardware variables. Note that the strings specified here as parameters
         // must match the names assigned in the robot controller configuration.
-        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("front_left");
-        DcMotor rearLeftMotor = hardwareMap.dcMotor.get("rear_left");
-        DcMotor frontRightMotor = hardwareMap.dcMotor.get("front_right");
-        DcMotor rearRightMotor = hardwareMap.dcMotor.get("rear_right");
-
-        // Set drive speed limiter (full power = 1.0)
-        final double DRIVE_SPEED_LIMITER = 0.6; // reduce drive rate to 60% of maximum
+        frontLeftMotor = hardwareMap.dcMotor.get("front_left");
+        rearLeftMotor = hardwareMap.dcMotor.get("rear_left");
+        frontRightMotor = hardwareMap.dcMotor.get("front_right");
+        rearRightMotor = hardwareMap.dcMotor.get("rear_right");
 
         // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
         // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
@@ -60,53 +61,44 @@ public class MecanumTeleOpRobotCentric extends LinearOpMode {
         // Run until the driver presses stop
         while (opModeIsActive()) {
 
-            // DRIVE = left joystick y axis (robot centric)
-            double drive = -gamepad1.left_stick_y;  // set as negative so pushing joystick forward is a positive value
+            // FORWARD/BACKWARD or LONGITUDINAL = left joystick y axis (robot centric)
+            double forward = -gamepad1.left_stick_y;  // set as negative so pushing joystick forward is a positive value
             // STRAFE = left joystick x axis
             double strafe = gamepad1.left_stick_x * 1.1;    // multiplier to counteract imperfect strafing, adjustable based on driver preference
             // TURN = right joystick x axis
             double turn = gamepad1.right_stick_x;
 
-            // Determine the largest motor power (absolute value) or 1. This ensures all the powers are scaled proportionally,
-            // but only if at least one is out of the range [-1.0, 1.0].
-            double kScaleFactor = Math.max(Math.abs(drive) + Math.abs(strafe) + Math.abs(turn), 1);
-
-            // The left Y component (y or DRIVE) is added to all wheels, the right X (rx or TURN) is added to the left wheels and subtracted from
-            // the right, and the left X component (x or STRAFE) is added to diagonal motors pairs (i.e., left front and right rear) and subtracted
-            // from the opposite diagonal pair (i.e., right front and left rear).
-            // see mecanum drive reference: https://cdn11.bigcommerce.com/s-x56mtydx1w/images/stencil/original/products/2234/13280/3209-0001-0007-Product-Insight-2__06708__33265.1725633323.png?c=1
-            double frontLeftPower = squarePower((drive + turn + strafe) / kScaleFactor);
-            double rearLeftPower = squarePower((drive + turn - strafe) / kScaleFactor);
-            double frontRightPower = squarePower((drive - turn - strafe) / kScaleFactor);
-            double rearRightPower = squarePower((drive - turn + strafe) / kScaleFactor);
-
-            // Set scaled motor powers (with limiter).
-            frontLeftMotor.setPower(frontLeftPower * DRIVE_SPEED_LIMITER);
-            rearLeftMotor.setPower(rearLeftPower * DRIVE_SPEED_LIMITER);
-            frontRightMotor.setPower(frontRightPower * DRIVE_SPEED_LIMITER);
-            rearRightMotor.setPower(rearRightPower * DRIVE_SPEED_LIMITER);
-
-            // Display power settings on Driver Hub
-            telemetry.addData("kScaleFactor", kScaleFactor);
-            telemetry.addLine()
-                    .addData("FrontLeft Power", "%4.2f", ((drive + turn + strafe) / kScaleFactor))
-                    .addData("FrontLeft Power²", "%4.2f", frontLeftPower);
-            telemetry.addLine()
-                    .addData("RearLeft Power", "%4.2f", ((drive + turn - strafe) / kScaleFactor))
-                    .addData("RearLeft Power²", "%s", rearLeftPower);
-            telemetry.addLine()
-                    .addData("FrontRight Power", "%4.2f", ((drive - turn - strafe) / kScaleFactor))
-                    .addData("FrontRight Power²", "%s", frontRightPower);
-            telemetry.addLine()
-                    .addData("RearRight Power", "%4.2f", ((drive - turn + strafe) / kScaleFactor))
-                    .addData("RearRight Power²", "%s", rearRightPower);
-            telemetry.update();
+            moveRobot(forward, strafe, turn, 0.6);  // speedLimiter reduces movement speed to a specified % of maximum (1.0).
         }
     }
 
+    public void moveRobot(double y, double x, double rx, double speedLimiter) {
+        // Function to move robot.
+        // Positive values of y move forward, positive values of x strafe to the right, positive values of rx rotate clockwise.
+    
+        // Determine the largest motor power (absolute value) or 1. This ensures all the powers are scaled proportionally,
+        // but only if at least one is out of the range [-1.0, 1.0].
+        double kScaleFactor = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+
+        // The left Y component (y or LONGITUDINAL) is added to all wheels, the left X component (x or STRAFE) is added
+        // to diagonal motors pairs (i.e., left front and right rear) and subtracted from the opposite diagonal pair
+        // (i.e., right front and left rear), and the right X (rx or TURN) is added to the left wheels and subtracted from the right.
+        // See mecanum drive reference: https://cdn11.bigcommerce.com/s-x56mtydx1w/images/stencil/original/products/2234/13280/3209-0001-0007-Product-Insight-2__06708__33265.1725633323.png?c=1
+        double frontLeftPower = squarePower((y + x + rx) / kScaleFactor);
+        double rearLeftPower = squarePower((y - x + rx) / kScaleFactor);
+        double frontRightPower = squarePower((y - x - rx) / kScaleFactor);
+        double rearRightPower = squarePower((y + x - rx) / kScaleFactor);
+    
+        // Set scaled motor powers (with limiter).
+        frontLeftMotor.setPower(frontLeftPower * speedLimiter);
+        rearLeftMotor.setPower(rearLeftPower * speedLimiter);
+        frontRightMotor.setPower(frontRightPower * speedLimiter);
+        rearRightMotor.setPower(rearRightPower * speedLimiter);
+    }
+    
     public static double squarePower(double power) {
         // Function to square motor power to allow for better micro control.  Returns a double.
-
+    
         if (power < 0) {    // check for negative power input
             return -Math.pow(power, 2);  // return negative result if negative power input
         } else {
